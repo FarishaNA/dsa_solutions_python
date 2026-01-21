de0e85561207df173381ac9d84429bebf30f3d6d
FarishNA <farishana426@gmail.com>
2026-01-21 19:53:42 +0530

    maximum subarray

---
+++ Arrays/Medium/053_maximum_subarray.py
@@ -0,0 +1,18 @@
+# LeetCode 53 – Maximum Subarray
+# https://leetcode.com/problems/maximum-subarray/
+# Uses Kadane’s Algorithm to find the maximum subarray sum in linear time.
+# Difficulty: Medium
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = max_sum = nums[0]
+
+        for num in nums[1:]:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+
+        return max_sum
